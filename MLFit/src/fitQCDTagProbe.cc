ed5e51b97395ad5cb85f747c33afa4245c66a47b
crovelli <>
2009-10-14 15:32:36 +0000

    new groups

--- MLFit/src/fitQCDTagProbe.cc
+++ MLFit/src/fitQCDTagProbe.cc
@@ -3,14 +3,12 @@ MLOptions GetDefaultOptions() {
   MLOptions opts;
   // Fit configuration
   opts.addBoolOption("useDeltaPhi",      "Use deltaphi",            kTRUE);        
-  opts.addBoolOption("useMET",           "Use MET",                 kFALSE);        
+  opts.addBoolOption("useMET",           "Use MET",                 kTRUE);        
   opts.addBoolOption("useMass",          "Use Mass",                kFALSE);        
   opts.addBoolOption("AllFit",           "Fit all species",         kFALSE);
   opts.addBoolOption("QCDOnlyFit",       "Fit QCD species only",    kFALSE);
   opts.addBoolOption("wenuOnlyFit",      "Fit W->wnu species only", kFALSE);
-  opts.addBoolOption("ttbarOnlyFit",     "Fit TTbar species only",  kFALSE);
-  opts.addBoolOption("zeeOnlyFit",       "Fit Z species only",      kTRUE);
-  // opts.addBoolOption("zAndGammaJOnlyFit", "Fit Z and gamma+jet species only", kTRUE);
+  opts.addBoolOption("ttbarOnlyFit",     "Fit TTbar species only",  kTRUE);
   return opts;
 }
 
@@ -29,30 +27,25 @@ void myFit() {
   RooRealVar *deltaphi = new RooRealVar("qcdDeltaphi","di-jet #Delta #phi",     1,0.5,3.1415);
   RooRealVar *met      = new RooRealVar("qcdMet",     "MET",                    10,0,100, "GeV");
   RooRealVar *mass     = new RooRealVar("qcdInvmass", "tag-probeinvariant mass",10,0,600, "GeV");
-  // RooRealVar *weight   = new RooRealVar("weight",     "weight",                 1);
+  RooRealVar *weight   = new RooRealVar("weight",     "weight",                 1);
   theFit.AddFlatFileColumn(deltaphi);
   theFit.AddFlatFileColumn(met);
-  theFit.AddFlatFileColumn(mass);
-  // theFit.AddFlatFileColumn(weight);
+  theFit.AddFlatFileColumn(mass);      
+  theFit.AddFlatFileColumn(weight);
   
   // define a fit model
   theFit.addModel("myFit", "Tag And Probe Zee");
   
   // define species
-  theFit.addSpecies("myFit", "sig",        "Signal Component");
-  theFit.addSpecies("myFit", "wenu",       "Wenu   Component");
-  theFit.addSpecies("myFit", "ttbar",      "TTbar  Component");
-  // theFit.addSpecies("myFit", "zAndGammaJ", "Zee and photon + jet   Component");
-  theFit.addSpecies("myFit", "zee",        "Zee Component");
+  theFit.addSpecies("myFit", "sig",    "Signal Component");
+  theFit.addSpecies("myFit", "wenu",   "Wenu   Component");
+  theFit.addSpecies("myFit", "ttbar",  "TTbar  Component");
 
   // deltaphi PDF
   if(opts.getBoolVal("useDeltaPhi")) {
-    theFit.addPdfWName("myFit", "sig" ,        "qcdDeltaphi",  "Poly3",   "sig_deltaphi");
-    theFit.addPdfWName("myFit", "wenu" ,       "qcdDeltaphi",  "Cruijff", "wenu_deltaphi");
-    theFit.addPdfWName("myFit", "ttbar" ,      "qcdDeltaphi",  "Poly3",   "ttbar_deltaphi");
-    // theFit.addPdfWName("myFit", "zee" ,        "qcdDeltaphi",  "Poly3",   "zee_deltaphi");
-    theFit.addPdfWName("myFit", "zee" ,        "qcdDeltaphi",  "Cruijff",  "zee_deltaphi");
-    // theFit.addPdfWName("myFit", "zAndGammaJ" , "qcdDeltaphi",  "Poly3", "zAndGammaJ_deltaphi");
+    theFit.addPdfWName("myFit", "sig" ,    "qcdDeltaphi",  "Poly3",   "sig_deltaphi");
+    theFit.addPdfWName("myFit", "wenu" ,   "qcdDeltaphi",  "Cruijff", "wenu_deltaphi");
+    theFit.addPdfWName("myFit", "ttbar" ,  "qcdDeltaphi",  "Poly3",   "ttbar_deltaphi");
   }
   
   // MET PDF
@@ -60,8 +53,6 @@ void myFit() {
     theFit.addPdfWName("myFit", "sig" ,        "qcdMet",  "Cruijff",  "sig_met");
     theFit.addPdfWName("myFit", "wenu" ,       "qcdMet",  "Cruijff",  "wenu_met");
     theFit.addPdfWName("myFit", "ttbar" ,      "qcdMet",  "Cruijff",  "ttbar_met");
-    theFit.addPdfWName("myFit", "zee" ,        "qcdMet",  "Cruijff",  "zee_met");
-    // theFit.addPdfWName("myFit", "zAndGammaJ" , "qcdMet",  "Cruijff",  "zAndGammaJ_met");
   }  
 
   // mass PDF
@@ -69,8 +60,6 @@ void myFit() {
     theFit.addPdfWName("myFit", "sig" ,        "qcdInvmass",  "Cruijff",  "sig_mass");
     theFit.addPdfWName("myFit", "wenu" ,       "qcdInvmass",  "Cruijff",  "wenu_mass");
     theFit.addPdfWName("myFit", "ttbar" ,      "qcdInvmass",  "Cruijff",  "ttbar_mass");
-    theFit.addPdfWName("myFit", "zee" ,        "qcdInvmass",  "Cruijff",  "zee_mass");
-    // theFit.addPdfWName("myFit", "zAndGammaJ" , "qcdInvmass",  "Cruijff",  "zAndGammaJ_mass");
   }  
 }
 
@@ -81,21 +70,19 @@ void myFit() {
 void FitQCDTagAndProbe() {
   
   myFit();
-
+  
   // Various fit options...
   MLOptions opts = GetDefaultOptions();
 
   // Load the data
   char datasetname[200];
   if(opts.getBoolVal("AllFit"))             sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/data.root");
-  if(opts.getBoolVal("QCDOnlyFit"))         sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/qcd_pt80.root");
+  if(opts.getBoolVal("QCDOnlyFit"))         sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/qcdSignal.root"); // QCD+gammaPlusJet 
   if(opts.getBoolVal("wenuOnlyFit"))        sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/wenu_5jobs.root");
   if(opts.getBoolVal("ttbarOnlyFit"))       sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/ttbar.root");
-  if(opts.getBoolVal("zeeOnlyFit"))         sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/zee.root");
-  // if(opts.getBoolVal("zAndGammaJOnlyFit"))  sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/zAndPhotonJet.root");
   theFit.addDataSetFromRootFile("T1", "T1", datasetname);
   RooDataSet *data = theFit.getDataSet("T1");
-  // data->setWeightVar("weight");
+  data->setWeightVar("weight");
   
   // build the fit likelihood
   RooAbsPdf *myPdf = theFit.buildModel("myFit");
@@ -105,8 +92,6 @@ void FitQCDTagAndProbe() {
   if(opts.getBoolVal("QCDOnlyFit"))        theFit.initialize("fitconfig/fit-qcdtagprobe-qcdonly.config");
   if(opts.getBoolVal("wenuOnlyFit"))       theFit.initialize("fitconfig/fit-qcdtagprobe-wenuonly.config");
   if(opts.getBoolVal("ttbarOnlyFit"))      theFit.initialize("fitconfig/fit-qcdtagprobe-ttbaronly.config");
-  if(opts.getBoolVal("zeeOnlyFit"))        theFit.initialize("fitconfig/fit-qcdtagprobe-zeeonly.config");
-  // if(opts.getBoolVal("zAndGammaJOnlyFit")) theFit.initialize("fitconfig/fit-qcdtagprobe-zAndGammaJonly.config");
   
   // Print Fit configuration 
   myPdf->getParameters(data)->selectByAttrib("Constant",kTRUE)->Print("V");
@@ -120,9 +105,6 @@ void FitQCDTagAndProbe() {
   if(opts.getBoolVal("QCDOnlyFit"))        theFit.writeConfigFile("fitres/fitMinimum-qcdtagprobe-qcdonly.config");  
   if(opts.getBoolVal("wenuOnlyFit"))       theFit.writeConfigFile("fitres/fitMinimum-qcdtagprobe-wenuonly.config");  
   if(opts.getBoolVal("ttbarOnlyFit"))      theFit.writeConfigFile("fitres/fitMinimum-qcdtagprobe-ttbaronly.config");  
-  if(opts.getBoolVal("zeeOnlyFit"))        theFit.writeConfigFile("fitres/fitMinimum-qcdtagprobe-zeeonly.config");  
-  // if(opts.getBoolVal("zAndGammaJOnlyFit")) theFit.writeConfigFile("fitres/fitMinimum-qcdtagprobe-zAndGammaJonly.config");  
-
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -137,15 +119,13 @@ void PlotQCDTagAndProbe(int nbins=19) {
   // Load the data
   char datasetname[200];
   if(opts.getBoolVal("AllFit"))             sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/data.root");
-  if(opts.getBoolVal("QCDOnlyFit"))         sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/qcd_pt80.root");
-  if(opts.getBoolVal("wenuOnlyFit"))        sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/wenu_5jobs.root");
+  if(opts.getBoolVal("QCDOnlyFit"))         sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/qcdSignal.root");
+  if(opts.getBoolVal("wenuOnlyFit"))        sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/wenu_5jobs.root"); 
   if(opts.getBoolVal("ttbarOnlyFit"))       sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/ttbar.root");
-  if(opts.getBoolVal("zeeOnlyFit"))         sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/zee.root");
-  // if(opts.getBoolVal("zAndGammaJOnlyFit"))  sprintf(datasetname,"/cmsrm/pc21/crovelli/data/Like3.2.X/datasets_QCDTaP/zAndPhotonJet.root");
   theFit.addDataSetFromRootFile("T1", "T1", datasetname);
 
   RooDataSet *data = theFit.getDataSet("T1");
-  // data->setWeightVar("weight");
+  data->setWeightVar("weight");
 
   // build the fit likelihood
   RooAbsPdf *myPdf = theFit.buildModel("myFit");
@@ -155,8 +135,6 @@ void PlotQCDTagAndProbe(int nbins=19) {
   if(opts.getBoolVal("QCDOnlyFit"))        theFit.initialize("fitres/fitMinimum-qcdtagprobe-qcdonly.config");  
   if(opts.getBoolVal("wenuOnlyFit"))       theFit.initialize("fitres/fitMinimum-qcdtagprobe-wenuonly.config");  
   if(opts.getBoolVal("ttbarOnlyFit"))      theFit.initialize("fitres/fitMinimum-qcdtagprobe-ttbaronly.config");  
-  if(opts.getBoolVal("zeeOnlyFit"))        theFit.initialize("fitres/fitMinimum-qcdtagprobe-zeeonly.config");  
-  // if(opts.getBoolVal("zAndGammaJOnlyFit")) theFit.initialize("fitres/fitMinimum-qcdtagprobe-zAndGammaJonly.config");  
 
   if(opts.getBoolVal("useDeltaPhi")) {
     TCanvas *c = new TCanvas("c","fitResult");
@@ -169,8 +147,6 @@ void PlotQCDTagAndProbe(int nbins=19) {
     if(opts.getBoolVal("QCDOnlyFit"))        c->SaveAs("fitoutput/deltaphi-qcdtagprobe-qcdonly.eps");
     if(opts.getBoolVal("wenuOnlyFit"))       c->SaveAs("fitoutput/deltaphi-qcdtagprobe-wenuonly.eps");
     if(opts.getBoolVal("ttbarOnlyFit"))      c->SaveAs("fitoutput/deltaphi-qcdtagprobe-ttbaronly.eps");
-    if(opts.getBoolVal("zeeOnlyFit"))        c->SaveAs("fitoutput/deltaphi-qcdtagprobe-zeeonly.eps");
-    // if(opts.getBoolVal("zAndGammaJOnlyFit")) c->SaveAs("fitoutput/deltaphi-qcdtagprobe-zAndGammaJonly.eps");
   }
 
   if(opts.getBoolVal("useMET")) {
@@ -184,8 +160,6 @@ void PlotQCDTagAndProbe(int nbins=19) {
     if(opts.getBoolVal("QCDOnlyFit"))        c->SaveAs("fitoutput/met-qcdtagprobe-qcdonly.eps");
     if(opts.getBoolVal("wenuOnlyFit"))       c->SaveAs("fitoutput/met-qcdtagprobe-wenuonly.eps");
     if(opts.getBoolVal("ttbarOnlyFit"))      c->SaveAs("fitoutput/met-qcdtagprobe-ttbaronly.eps");
-    if(opts.getBoolVal("zeeOnlyFit"))        c->SaveAs("fitoutput/met-qcdtagprobe-zeeonly.eps");
-    // if(opts.getBoolVal("zAndGammaJOnlyFit")) c->SaveAs("fitoutput/met-qcdtagprobe-zAndGammaJonly.eps");
   }
 
   if(opts.getBoolVal("useMass")) {
@@ -200,8 +174,6 @@ void PlotQCDTagAndProbe(int nbins=19) {
     if(opts.getBoolVal("QCDOnlyFit"))        c->SaveAs("fitoutput/mass-qcdtagprobe-qcdonly.eps");
     if(opts.getBoolVal("wenuOnlyFit"))       c->SaveAs("fitoutput/mass-qcdtagprobe-wenuonly.eps");
     if(opts.getBoolVal("ttbarOnlyFit"))      c->SaveAs("fitoutput/mass-qcdtagprobe-ttbaronly.eps");
-    if(opts.getBoolVal("zeeOnlyFit"))        c->SaveAs("fitoutput/mass-qcdtagprobe-zeeonly.eps");
-    //if(opts.getBoolVal("zAndGammaJOnlyFit")) c->SaveAs("fitoutput/mass-qcdtagprobe-zAndGammaJonly.eps");
   }
 
 }
@@ -226,13 +198,10 @@ RooPlot *MakePlot(TString VarName, MLFit* theFit, RooDataSet* theData, int nbins
   RooAbsPdf *thePdf = theFit->getPdf("myFit");
   thePdf->plotOn(plot, RooFit::LineColor(kBlack));
 
-  double Ns          = theFit->getRealPar("N_sig")->getVal();
-  double Nwenu       = theFit->getRealPar("N_wenu")->getVal();
-  double Nttbar      = theFit->getRealPar("N_ttbar")->getVal();
-  // double NzAndGammaJ = theFit->getRealPar("N_zAndGammaJ")->getVal();
-  double Nzee        = theFit->getRealPar("N_zee")->getVal();
-  // double Nb = Nwenu + Nttbar + NzAndGammaJ;
-  double Nb = Nwenu + Nttbar + Nzee;
+  double Ns     = theFit->getRealPar("N_sig")->getVal();
+  double Nwenu  = theFit->getRealPar("N_wenu")->getVal();
+  double Nttbar = theFit->getRealPar("N_ttbar")->getVal();
+  double Nb     = Nwenu + Nttbar;
 
   // plot (dashed) the bkg component
   theFit->getRealPar("N_sig")->setVal(0.);
