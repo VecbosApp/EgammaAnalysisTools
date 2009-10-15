77292991c99db99e6ed34a55b414313ff898fb25
emanuele <>
2009-10-15 16:22:56 +0000

    3 is the sPlots comparison

--- include/Application.hh
+++ include/Application.hh
@@ -2,6 +2,6 @@
 // 
 //   1 = Likelihood analysis
 //   2 = Pdfs Maker from Z tag and probe
-//   3 = Isolation Pdfs from Z tag and probe
+//   3 = compare Pdfs in MC and data sPlots
 
 #define Application 2
