4eb406e15e226f0888da798116ebfe15d56967bf
emanuele <>
2010-04-20 17:12:30 +0000

    update for egamma analysis

--- cmst3_submit_manyfilesperjob.py
+++ cmst3_submit_manyfilesperjob.py
@@ -5,41 +5,49 @@ import sys
 #######################################
 ### usage  cmst3_submit_manyfilesperjob.py dataset njobs applicationName queue 
 #######################################
-if len(sys.argv) != 5:
-    print "usage cmst3_submit_manyfilesperjob.py dataset njobs applicationName queue"
+if len(sys.argv) != 7:
+    print "usage cmst3_submit_manyfilesperjob.py process dataset njobs applicationName sample queue"
     sys.exit(1)
-dataset = sys.argv[1]
-inputlist = "castor_OctX/"+dataset+".list"
+process = sys.argv[1]
+dataset = sys.argv[2]
+inputlist = "../cmst3_33X/"+process+"/"+dataset+".list"
+#settingfile = "config/RSZZsettings.txt"
 output = dataset
-queue = sys.argv[4]
-ijobmax = int(sys.argv[2])
-application = sys.argv[3]
+# choose among cmt3 8nm 1nh 8nh 1nd 1nw 
+#queue = "cmst3"
+#queue = "cms8nht3"
+queue = sys.argv[6]
+#ijobmax = 40
+ijobmax = int(sys.argv[3])
+#application = "VecbosApp"
+application = sys.argv[4]
+sample = sys.argv[5]
 # to write on the cmst3 cluster disks
 ################################################
 #castordir = "/castor/cern.ch/user/m/mpierini/CMST3/Vecbos/output/"
 #outputmain = castordir+output
 # to write on local disks
 ################################################
-castordir = "/castor/cern.ch/user/XXX/"
-diskoutputdir = "/cmsrm/pc21/crovelli/data/Like3.2.X/results1/"
-outputmain = castordir+output
-diskoutputmain = diskoutputdir+output
+castordir = "/castor/cern.ch/user/e/emanuele/VecBos33X/SCStudy/"
+diskoutputdir = "/cmsrm/pc21_2/emanuele/data/VecBos3.3.X/SCStudy/"
+outputmain = castordir+"/"+process+"/"+output+"/"+sample
+diskoutputmain = diskoutputdir+"/"+process+"/"+output+"/"+sample
 # prepare job to write on the cmst3 cluster disks
 ################################################
-os.system("mkdir "+output)
-os.system("mkdir "+output+"/log/")
-os.system("mkdir "+output+"/input/")
-os.system("mkdir "+output+"/src/")
+os.system("mkdir -p "+process+"/"+output+"/"+sample)
+os.system("mkdir -p "+process+"/"+output+"/"+sample+"/log/")
+os.system("mkdir -p "+process+"/"+output+"/"+sample+"/input/")
+os.system("mkdir -p "+process+"/"+output+"/"+sample+"/src/")
 outputroot = outputmain+"/root/"
 if castordir != "none": 
-    os.system("rfmkdir "+outputmain)
-    os.system("rfmkdir "+outputroot)
+    os.system("rfmkdir -p "+outputmain)
+    os.system("rfmkdir -p "+outputroot)
     os.system("rfchmod 777 "+outputmain)
     os.system("rfchmod 777 "+outputroot)
-else: os.system("mkdir "+outputroot)
+else: os.system("mkdir -p "+outputroot)
 
 if diskoutputdir != "none": 
-    os.system("ssh -o BatchMode=yes -o StrictHostKeyChecking=no pccmsrm21 mkdir "+diskoutputmain)
+    os.system("ssh -o BatchMode=yes -o StrictHostKeyChecking=no pccmsrm21 mkdir -p "+diskoutputmain)
 
 #look for the current directory
 #######################################
@@ -53,7 +61,7 @@ input = open(inputlist)
 
 for ijob in range(ijobmax):
     # prepare the list file
-    inputfilename = pwd+"/"+output+"/input/input_"+str(ijob)+".list"
+    inputfilename = pwd+"/"+process+"/"+output+"/"+sample+"/input/input_"+str(ijob)+".list"
     inputfile = open(inputfilename,'w')
     # if it is a normal job get filesperjob lines
     if ijob != (ijobmax-1):
@@ -71,18 +79,21 @@ for ijob in range(ijobmax):
     inputfile.close()
 
     # prepare the script to run
-    outputname = output+"/src/submit_"+str(ijob)+".src"
+    outputname = process+"/"+output+"/"+sample+"/src/submit_"+str(ijob)+".src"
     outputfile = open(outputname,'w')
     outputfile.write('#!/bin/bash\n')
     outputfile.write('export STAGE_HOST=castorcms\n')
-    #outputfile.write('export STAGE_SVCCLASS=cmst3\n')
+    outputfile.write('export STAGE_SVCCLASS=cmst3\n')
+    #    outputfile.write('cd '+pwd)
     outputfile.write('cp -r '+pwd+'/config $WORKDIR\n')
     outputfile.write('cd $WORKDIR\n')
-    outputfile.write(pwd+'/'+application+' '+inputfilename+" "+output+"_"+str(ijob)+" "+"\n")
-    outputfile.write('rfcp *.root '+outputroot+'\n')
-    outputfile.write('scp -o BatchMode=yes -o StrictHostKeyChecking=no *.root pccmsrm21:'+diskoutputmain+'/{}\n') 
+    outputfile.write(pwd+'/'+application+' '+inputfilename+" "+output+"_"+str(ijob)+"_ "+sample+"\n")
+#    if castordir != "none": outputfile.write('./VecbosApp '+inputfilename+" "+" rfio://"+outputroot+output+"_"+str(ijob)+".root\n")
+#    else:  
+    outputfile.write('ls *.root | xargs -i rfcp {} '+outputroot+'\n')
+    outputfile.write('ls *.root | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm21:'+diskoutputmain+'/{}\n') 
     outputfile.close
-    os.system("echo bsub -q "+queue+" -o "+output+"/log/"+output+"_"+str(ijob)+".log source "+pwd+"/"+outputname)
-    os.system("bsub -q "+queue+" -o "+output+"/log/"+output+"_"+str(ijob)+".log source "+pwd+"/"+outputname)
+    os.system("echo bsub -q "+queue+" -o "+output+"/"+sample+"/log/"+output+"_"+str(ijob)+".log source "+pwd+"/"+outputname)
+    os.system("bsub -q "+queue+" -o "+process+"/"+output+"/"+sample+"/log/"+output+"_"+str(ijob)+".log source "+pwd+"/"+outputname)
     ijob = ijob+1
     continue
