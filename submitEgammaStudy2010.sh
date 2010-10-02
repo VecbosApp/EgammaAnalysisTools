2ec371298eac0b19917908ad3c3fcf9db939beee
emanuele <>
2010-10-02 11:11:36 +0000

    This commit was manufactured by cvs2svn to create tag 'edm-20Oct2010'.
    Sprout from master 2010-10-02 11:11:34 UTC emanuele 'change comment'
    Cherrypick from master 2010-04-20 17:12:52 UTC emanuele 'first commit':
        cmst3_submit_manyfilesperjob.py
        submitEgammaStudy2010.sh
    Delete:
        cmst3_submit_manyfilesperjob_data38.py
        macro/EgammaEventCounter.cc
        macro/EgammaYieldsSteps.cc
        macro/addWeightsToEgammaTree.cc
        macro/checkGammaJet.C
        macro/checkGammaJet.h
        macro/createFitDatasetsEG.cc
        macro/createFitDatasetsEG.sh
        macro/makeDataMCPlots.C
        macro/mergeMultiSamples.sh
        macro/mergeTrees.sh
        macro/weightTrees.sh
        macro/weights.cc
        submitData2010_38X.sh

--- submitEgammaStudy2010.sh
+++ submitEgammaStudy2010.sh
@@ -1,35 +1,41 @@
 #!/bin/sh
 
-echo "SUBMITTING PHOTON + JETS..."
-python cmst3_submit_manyfilesperjob.py PhotonJet PhotonJet_Pt0to15    1 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py PhotonJet PhotonJet_Pt120to170 1 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py PhotonJet PhotonJet_Pt15to20   1 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py PhotonJet PhotonJet_Pt170to300 1 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py PhotonJet PhotonJet_Pt20to30   1 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py PhotonJet PhotonJet_Pt300to500 1 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py PhotonJet PhotonJet_Pt30to50   1 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py PhotonJet PhotonJet_Pt500toInf 1 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py PhotonJet PhotonJet_Pt50to80   1 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py PhotonJet PhotonJet_Pt80to120  1 EgammaAnalysis 4 8nh
-echo "DONE PHOTON + JETS."
-
-echo "submitting W+jets, Z+jets"
-python cmst3_submit_manyfilesperjob.py WJetsMADGRAPH WJets-madgraph 20 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py ZJetsMADGRAPH ZJets-madgraph  2 EgammaAnalysis 4 8nh
-echo "done with W+jets"
+echo "submitting signals..."
+python cmst3_submit_manyfilesperjob.py WJetsMADGRAPH WJetsMADGRAPH 206 EgammaAnalysisTools 0 8nh
+python cmst3_submit_manyfilesperjob.py WJetsMADGRAPH WJetsMADGRAPH 206 EgammaAnalysisTools 2 8nh
+python cmst3_submit_manyfilesperjob.py ZJetsMADGRAPH ZJetsMADGRAPH 23 EgammaAnalysisTools 1 8nh
+python cmst3_submit_manyfilesperjob.py ZJetsMADGRAPH ZJetsMADGRAPH 23 EgammaAnalysisTools 3 8nh
+echo "done with signals."
 
 echo "submitting QCD di-jets..."
-python cmst3_submit_manyfilesperjob.py QCD QCD_BCtoE_Pt20to30        5 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py QCD QCD_BCtoE_Pt30to80        5 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py QCD QCD_BCtoE_Pt80to170       5 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py QCD QCD_EMEnriched_Pt20to30 106 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py QCD QCD_EMEnriched_Pt30to80 121 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py QCD QCD_EMEnriched_Pt80to170 10 EgammaAnalysis 4 8nh
+python cmst3_submit_manyfilesperjob.py QCD QCD_BCtoE_Pt20to30 28 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py QCD QCD_BCtoE_Pt30to80 63 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py QCD QCD_BCtoE_Pt80to170 33 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py QCD QCD_EMEnriched_Pt20to30 281 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py QCD QCD_EMEnriched_Pt30to80 276 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py QCD QCD_EMEnriched_Pt80to170 134 EgammaAnalysisTools 4 8nh
 echo "done QCD di-jets."
 
-echo "SUBMITTING TTBAR and single top"
-python cmst3_submit_manyfilesperjob.py TTbar TTbarJets-madgraph 3 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py SingleTop SingleTop_sChannel-madgraph  1 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py SingleTop SingleTop_tChannel-madgraph  1 EgammaAnalysis 4 8nh
-python cmst3_submit_manyfilesperjob.py SingleTop SingleTop_tWChannel-madgraph 1 EgammaAnalysis 4 8nh
-echo "DONE TTBAR and SINGLE TOP."
+echo "SUBMITTING PHOTON + JETS..."
+python cmst3_submit_manyfilesperjob.py PhotonJet Pt0to15 5 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py PhotonJet Pt120to170 5 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py PhotonJet Pt15to20 5 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py PhotonJet Pt170to300 5 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py PhotonJet Pt20to30 5 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py PhotonJet Pt300to500 5 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py PhotonJet Pt30to50 5 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py PhotonJet Pt500toInf 5 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py PhotonJet Pt50to80 5 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py PhotonJet Pt80to120 5 EgammaAnalysisTools 4 8nh
+echo "DONE PHOTON + JETS."
+
+echo "SUBMITTING TTBAR (MC@NLO)..."
+python cmst3_submit_manyfilesperjob.py TTbar mcatnlo 99 EgammaAnalysisTools 4 8nh
+echo "DONE TTBAR (MC@NLO)."
+
+echo "SUBMITTING SINGLE TOP..."
+python cmst3_submit_manyfilesperjob.py SingleTop SingleTop_sChannel_madgraph 10 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py SingleTop SingleTop_tChannel_madgraph 10 EgammaAnalysisTools 4 8nh
+python cmst3_submit_manyfilesperjob.py SingleTop SingleTop_tWChannel_madgraph 10 EgammaAnalysisTools 4 8nh
+echo "DONE SINGLE TOP."
+
