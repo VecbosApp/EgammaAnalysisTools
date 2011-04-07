1c7913d12f9251342c33609835fa31a5ee3ba025
emanuele <>
2011-04-07 13:03:18 +0000

    other categorization

--- src/sPlotsPdfsComparison.cc
+++ src/sPlotsPdfsComparison.cc
@@ -34,6 +34,7 @@ void sPlotsPdfsComparison::Loop()
   defaultSwitches.m_useFBrem = true;
   defaultSwitches.m_useEoverP = false;
   defaultSwitches.m_useSigmaPhiPhi = true;
+  defaultSwitches.m_useHoverE = false;
   LH = new ElectronLikelihood(&(*LHdir), &(*LHdir), &(*LHdir), &(*LHdir),
                               defaultSwitches, std::string("class"),std::string("class"),true,true);
 
@@ -42,7 +43,7 @@ void sPlotsPdfsComparison::Loop()
   int firstEvent = 0;
   int lastEvent  = nentries;
   cout << firstEvent << " " << lastEvent << endl;
-  
+
   Long64_t nbytes = 0, nb = 0;
   for (Long64_t jentry=firstEvent; jentry<lastEvent;jentry++) {
     Long64_t ientry = LoadTree(jentry);
@@ -51,18 +52,19 @@ void sPlotsPdfsComparison::Loop()
     if (jentry%10000 == 0) std::cout << ">>> Processing event # " << jentry << " / " << nentries << std::endl;
 
     // da cambiare (solo x dati, selezionare sgn o fondo)
-    float weight = (m_doSignal) ? N_sig_sw : N_qcd_sw;
+    //    float weight = (m_doSignal) ? N_sig_sw : N_qcd_sw;
     float wgt = (m_isMC) ? f_weight : weight;
-    if(m_isDataZTaP) wgt = 1.0;
+    //    float wgt = 1.0;
+    //    if(m_isDataZTaP) wgt = 1.0;
     // float wgt = (m_isMC) ? 1.0 : N_qcd_sw;
-    
+
     int jecal, jptbin, jclass;
     if(!m_isDataZTaP) { // W->enu sample
       if(m_isMC) {
         if(wgt>500) continue;
-        if (fabs(f_eta)<1.479)  jecal = 0;
-        else if (fabs(f_eta)>=1.479) jecal = 1;
-        else continue;
+        if(fabs(f_eta)<0.8) jecal = 0;
+        else if (fabs(f_eta)<1.479) jecal = 1;
+        else jecal = 2;
         if(f_pt>=10 && f_pt<=20) jptbin = 0;
         else if(f_pt>20) jptbin = 1;
         else continue;
@@ -71,37 +73,36 @@ void sPlotsPdfsComparison::Loop()
 
         if(f_nbrem==0) jclass = 0;
         else jclass = 1;
-        if(m_doSignal) {
-          if(jptbin==1 && (f_nJets>0 || f_pfmet/f_pt<0.3 || f_isIsoWP80<0)) continue;
-        }
+//         if(m_doSignal) {
+//           if(f_nJets>0 || f_pfmet/f_pt<0.3 || f_isIsoWP80<0) continue;
+//         }
       } else {
         if(see<1e-4) continue; // remove residual spikes (if the cleaning is not done)
-        if (fabs(eta)<1.479)  jecal = 0;
-        else if (fabs(eta)>=1.479) jecal = 1;
-        else continue;
+        if(fabs(f_eta)<0.8) jecal = 0;
+        else if (fabs(f_eta)<1.479) jecal = 1;
+        else jecal = 2;
         if(pt>=10 && pt<=20) jptbin = 0;
         else if(pt>20) jptbin = 1;
         else continue;
         if(nbrem==0) jclass = 0;
         else jclass = 1;
         if(m_doSignal) {
-          if(nJets>0 || pfmet/pt<0.3 || isIsoWP80<0) continue;
+          //          if(nJets>0 || pfmet/pt<0.3 || isIsoWP80<0) continue;
         }
       }
-
       if(m_isDataZTaP) {
-        if (fabs(ztap_eta)<1.479)  jecal = 0;
-        else if (fabs(ztap_eta)>=1.479) jecal = 1;
-        else continue;
+        if(fabs(ztap_eta)<0.8) jecal = 0;
+        else if (fabs(ztap_eta)<1.479) jecal = 1;
+        else jecal = 2;
       }
 
       // populate the unusued PDFs with uniform distribution (including the under/over-flows)
-      float minSPP = sigmaIPhiIPhiClassEle [jecal][jptbin][jclass]->GetXaxis()->GetBinLowEdge(0)-1.0;
-      float maxSPP = sigmaIPhiIPhiClassEle [jecal][jptbin][jclass]->GetXaxis()->GetBinUpEdge(sigmaIPhiIPhiClassEle [jecal][jptbin][jclass]->GetNbinsX()+1)+1.0;
-      float minFBrem = fBremClassEle [jecal][jptbin][jclass]->GetXaxis()->GetBinLowEdge(0)-1.0;
-      float maxFBrem = fBremClassEle [jecal][jptbin][jclass]->GetXaxis()->GetBinUpEdge(fBremClassEle [jecal][jptbin][jclass]->GetNbinsX()+1)+1.0;
+      float minSPP = sigmaIPhiIPhiClassEle [jecal][jptbin][jclass]->GetXaxis()->GetBinLowEdge(0)-1.0E-03;
+      float maxSPP = sigmaIPhiIPhiClassEle [jecal][jptbin][jclass]->GetXaxis()->GetBinUpEdge(sigmaIPhiIPhiClassEle [jecal][jptbin][jclass]->GetNbinsX()+1)+1.0E-03;
+      float minFBrem = fBremClassEle [jecal][jptbin][jclass]->GetXaxis()->GetBinLowEdge(0)-1.0E-03;
+      float maxFBrem = fBremClassEle [jecal][jptbin][jclass]->GetXaxis()->GetBinUpEdge(fBremClassEle [jecal][jptbin][jclass]->GetNbinsX()+1)+1.0E-03;
       TRandom3 rnd(jentry);
-     
+
       if(m_isMC) {
         dPhiEle          [jecal] -> Fill ( f_dphi, wgt );
         dEtaEle          [jecal] -> Fill ( f_deta, wgt );
@@ -122,12 +123,13 @@ void sPlotsPdfsComparison::Loop()
         sigmaIPhiIPhiUnsplitEle [jecal][jptbin] -> Fill ( f_spp, wgt ); 
         fBremUnsplitEle [jecal][jptbin] -> Fill ( f_fbrem, wgt );
 
+
         dPhiClassEle [jecal][jptbin][jclass] -> Fill ( f_dphi, wgt );
         dEtaClassEle [jecal][jptbin][jclass] -> Fill ( f_deta, wgt );
         EoPClassEle [jecal][jptbin][jclass] -> Fill ( f_eop, wgt );
         HoEClassEle  [jecal][jptbin][jclass] -> Fill ( f_hoe, wgt );
         sigmaIEtaIEtaClassEle [jecal][jptbin][jclass] -> Fill ( f_see, wgt );
-            
+        
         if(jclass==0) {
           sigmaIPhiIPhiClassEle [jecal][jptbin][jclass] -> Fill ( f_spp, wgt ); 
           fBremClassEle [jecal][jptbin][jclass] -> Fill ( rnd.Uniform(minFBrem,maxFBrem) );
@@ -174,9 +176,9 @@ void sPlotsPdfsComparison::Loop()
       lhUnsplitEle [jecal][jptbin] -> Fill ( f_lh, wgt );
       lhClassEle [jecal][jptbin][jclass] -> Fill ( f_lh, wgt );
     } else {
-      if (fabs(ztap_eta)<1.479)  jecal = 0;
-      else if (fabs(ztap_eta)>=1.479) jecal = 1;
-      else continue;
+      if(fabs(ztap_eta)<0.8) jecal = 0;
+      else if (fabs(ztap_eta)<1.479) jecal = 1;
+      else jecal = 2;
       dPhiEle          [jecal] -> Fill ( ztap_dphi, wgt );
       dEtaEle          [jecal] -> Fill ( ztap_deta, wgt );
       EoPEle           [jecal] -> Fill ( ztap_eop, wgt );
@@ -212,7 +214,7 @@ void sPlotsPdfsComparison::Loop()
 
   etaEle->Write();
   
-  for (int jecal=0; jecal<2; jecal++) {
+  for (int jecal=0; jecal<3; jecal++) {
     dPhiEle[jecal]->Write();
     dEtaEle[jecal]->Write();
     EoPEle[jecal]->Write();           
@@ -230,7 +232,7 @@ void sPlotsPdfsComparison::Loop()
   // ROOT file with Electron Likelihood PDFs
   TFile *fileOut2 = TFile::Open(bufferLikelihood, "recreate");
   
-  for (int iecal=0; iecal<2; iecal++) {
+  for (int iecal=0; iecal<3; iecal++) {
 
     for(int iptbin=0; iptbin<2; iptbin++) {
       
@@ -285,7 +287,7 @@ void sPlotsPdfsComparison::bookHistosVariableBinning() {
   // iecal = 0 --> barrel
   // iecal = 1 --> endcap
   char histo[200];
-  for (int iecal=0; iecal<2; iecal++) {
+  for (int iecal=0; iecal<3; iecal++) {
       
     sprintf(histo,"dPhiClass_electrons_%d",iecal);
     dPhiEle[iecal] = new TH1F(histo, histo, 10, dPhiBins);
@@ -304,6 +306,12 @@ void sPlotsPdfsComparison::bookHistosVariableBinning() {
   sigmaIEtaIEtaEle[iecal] = new TH1F(histo, histo, 13, seeBinsEB);
 
   iecal = 1;
+  sprintf(histo,"EoPClass_electrons_%d",iecal);
+  EoPEle[iecal] = new TH1F(histo, histo, 11, EoPBinsEB);
+  sprintf(histo,"sigmaIEtaIEtaClass_electrons_%d",iecal);
+  sigmaIEtaIEtaEle[iecal] = new TH1F(histo, histo, 13, seeBinsEB);
+
+  iecal = 2;
   sprintf(histo,"EoPClass_electrons_%d",iecal);
   EoPEle[iecal] = new TH1F(histo, histo, 11, EoPBinsEE);
   sprintf(histo,"sigmaIEtaIEtaClass_electrons_%d",iecal);
@@ -326,7 +334,7 @@ void sPlotsPdfsComparison::bookHistosFixedBinning() {
   etaEle->Sumw2();
 
   if(m_doSignal) {
-    // EB histograms
+    // EB histograms |eta|<0.8
     sprintf(histo,"dPhiClass_electrons_%d",0);
     dPhiEle[0] = new TH1F(histo, histo, 50, -0.05, 0.05);
     dPhiEle[0]->Sumw2();
@@ -355,24 +363,24 @@ void sPlotsPdfsComparison::bookHistosFixedBinning() {
     lhEle[0] = new TH1F(histo, histo, 50, 0., 1.);
     lhEle[0]->Sumw2();
 
-    // EE histograms
+    // EB histograms |eta|>0.8
     sprintf(histo,"dPhiClass_electrons_%d",1);
-    dPhiEle[1] = new TH1F(histo, histo, 50, -0.1, 0.1);
+    dPhiEle[1] = new TH1F(histo, histo, 50, -0.05, 0.05);
     dPhiEle[1]->Sumw2();
     sprintf(histo,"dEtaClass_electrons_%d",1);
-    dEtaEle[1] = new TH1F(histo, histo, 50, -0.03, 0.03);
+    dEtaEle[1] = new TH1F(histo, histo, 50, -0.008, 0.008);
     dEtaEle[1]->Sumw2();
     sprintf(histo,"HoEClass_electrons_%d",1);
     HoEEle[1] = new TH1F(histo, histo, 50, 0., 0.15);
     HoEEle[1]->Sumw2();
     sprintf(histo,"EoPClass_electrons_%d",1);
-    EoPEle[1] = new TH1F(histo, histo, 50, 0., 5.);
+    EoPEle[1] = new TH1F(histo, histo, 50, 0., 3.);
     EoPEle[1]->Sumw2();
     sprintf(histo,"sigmaIEtaIEtaClass_electrons_%d",1);
-    sigmaIEtaIEtaEle[1] = new TH1F(histo, histo, 50, 0.01, 0.05);
+    sigmaIEtaIEtaEle[1] = new TH1F(histo, histo, 50, 0., 0.03);
     sigmaIEtaIEtaEle[1]->Sumw2();
     sprintf(histo,"fbremClass_electrons_%d",1);
-    fbremEle[1] = new TH1F(histo, histo, 30, -1.0, 1.0);
+    fbremEle[1] = new TH1F(histo, histo, 40, -1.0, 1.0);
     fbremEle[1]->Sumw2();
     sprintf(histo,"phiClass_electrons_%d",1);
     phiEle[1] = new TH1F(histo, histo, 50, 0., 2*TMath::Pi());
@@ -383,8 +391,38 @@ void sPlotsPdfsComparison::bookHistosFixedBinning() {
     sprintf(histo,"lhClass_electrons_%d",1);
     lhEle[1] = new TH1F(histo, histo, 50, 0., 1.);
     lhEle[1]->Sumw2();
+
+
+    // EE histograms
+    sprintf(histo,"dPhiClass_electrons_%d",1);
+    dPhiEle[2] = new TH1F(histo, histo, 50, -0.1, 0.1);
+    dPhiEle[2]->Sumw2();
+    sprintf(histo,"dEtaClass_electrons_%d",1);
+    dEtaEle[2] = new TH1F(histo, histo, 50, -0.03, 0.03);
+    dEtaEle[2]->Sumw2();
+    sprintf(histo,"HoEClass_electrons_%d",1);
+    HoEEle[2] = new TH1F(histo, histo, 50, 0., 0.15);
+    HoEEle[2]->Sumw2();
+    sprintf(histo,"EoPClass_electrons_%d",1);
+    EoPEle[2] = new TH1F(histo, histo, 50, 0., 5.);
+    EoPEle[2]->Sumw2();
+    sprintf(histo,"sigmaIEtaIEtaClass_electrons_%d",1);
+    sigmaIEtaIEtaEle[2] = new TH1F(histo, histo, 50, 0.01, 0.05);
+    sigmaIEtaIEtaEle[2]->Sumw2();
+    sprintf(histo,"fbremClass_electrons_%d",1);
+    fbremEle[2] = new TH1F(histo, histo, 30, -1.0, 1.0);
+    fbremEle[2]->Sumw2();
+    sprintf(histo,"phiClass_electrons_%d",1);
+    phiEle[2] = new TH1F(histo, histo, 50, 0., 2*TMath::Pi());
+    phiEle[2]->Sumw2();
+    sprintf(histo,"chargeClass_electrons_%d",1);
+    chargeEle[2] = new TH1F(histo, histo, 2, -2., 2.);
+    chargeEle[2]->Sumw2();
+    sprintf(histo,"lhClass_electrons_%d",1);
+    lhEle[2] = new TH1F(histo, histo, 50, 0., 1.);
+    lhEle[2]->Sumw2();
   } else {
-    // EB histograms
+    // EB histograms |eta|<0.8
     sprintf(histo,"dPhiClass_electrons_%d",0);
     dPhiEle[0] = new TH1F(histo, histo, 50, -0.15, 0.15);
     dPhiEle[0]->Sumw2();
@@ -413,34 +451,63 @@ void sPlotsPdfsComparison::bookHistosFixedBinning() {
     lhEle[0] = new TH1F(histo, histo, 50, 0., 1.);
     lhEle[0]->Sumw2();
 
-    // EE histograms
-    sprintf(histo,"dPhiClass_electrons_%d",1);
-    dPhiEle[1] = new TH1F(histo, histo, 50, -0.3, 0.3);
+    // EB histograms |eta|>=0.8
+    sprintf(histo,"dPhiClass_electrons_%d",0);
+    dPhiEle[1] = new TH1F(histo, histo, 50, -0.15, 0.15);
     dPhiEle[1]->Sumw2();
-    sprintf(histo,"dEtaClass_electrons_%d",1);
+    sprintf(histo,"dEtaClass_electrons_%d",0);
     dEtaEle[1] = new TH1F(histo, histo, 50, -0.02, 0.02);
     dEtaEle[1]->Sumw2();
-    sprintf(histo,"HoEClass_electrons_%d",1);
+    sprintf(histo,"HoEClass_electrons_%d",0);
     HoEEle[1] = new TH1F(histo, histo, 50, 0., 0.15);
     HoEEle[1]->Sumw2();
-    sprintf(histo,"EoPClass_electrons_%d",1);
+    sprintf(histo,"EoPClass_electrons_%d",0);
     EoPEle[1] = new TH1F(histo, histo, 50, 0., 10.);
     EoPEle[1]->Sumw2();
-    sprintf(histo,"sigmaIEtaIEtaClass_electrons_%d",1);
-    sigmaIEtaIEtaEle[1] = new TH1F(histo, histo, 50, 0.01, 0.08);
+    sprintf(histo,"sigmaIEtaIEtaClass_electrons_%d",0);
+    sigmaIEtaIEtaEle[1] = new TH1F(histo, histo, 50, 0., 0.03);
     sigmaIEtaIEtaEle[1]->Sumw2();
-    sprintf(histo,"fbremClass_electrons_%d",1);
+    sprintf(histo,"fbremClass_electrons_%d",0);
     fbremEle[1] = new TH1F(histo, histo, 50, -1.0, 1.0);
     fbremEle[1]->Sumw2();
-    sprintf(histo,"phiClass_electrons_%d",1);
+    sprintf(histo,"phiClass_electrons_%d",0);
     phiEle[1] = new TH1F(histo, histo, 50, 0., 2*TMath::Pi());
     phiEle[1]->Sumw2();
-    sprintf(histo,"chargeClass_electrons_%d",1);
+    sprintf(histo,"chargeClass_electrons_%d",0);
     chargeEle[1] = new TH1F(histo, histo, 2, -2., 2.);
     chargeEle[1]->Sumw2();
-    sprintf(histo,"lhClass_electrons_%d",1);
+    sprintf(histo,"lhClass_electrons_%d",0);
     lhEle[1] = new TH1F(histo, histo, 50, 0., 1.);
     lhEle[1]->Sumw2();
+
+    // EE histograms
+    sprintf(histo,"dPhiClass_electrons_%d",1);
+    dPhiEle[2] = new TH1F(histo, histo, 50, -0.3, 0.3);
+    dPhiEle[2]->Sumw2();
+    sprintf(histo,"dEtaClass_electrons_%d",1);
+    dEtaEle[2] = new TH1F(histo, histo, 50, -0.02, 0.02);
+    dEtaEle[2]->Sumw2();
+    sprintf(histo,"HoEClass_electrons_%d",1);
+    HoEEle[2] = new TH1F(histo, histo, 50, 0., 0.15);
+    HoEEle[2]->Sumw2();
+    sprintf(histo,"EoPClass_electrons_%d",1);
+    EoPEle[2] = new TH1F(histo, histo, 50, 0., 10.);
+    EoPEle[2]->Sumw2();
+    sprintf(histo,"sigmaIEtaIEtaClass_electrons_%d",1);
+    sigmaIEtaIEtaEle[2] = new TH1F(histo, histo, 50, 0.01, 0.08);
+    sigmaIEtaIEtaEle[2]->Sumw2();
+    sprintf(histo,"fbremClass_electrons_%d",1);
+    fbremEle[2] = new TH1F(histo, histo, 50, -1.0, 1.0);
+    fbremEle[2]->Sumw2();
+    sprintf(histo,"phiClass_electrons_%d",1);
+    phiEle[2] = new TH1F(histo, histo, 50, 0., 2*TMath::Pi());
+    phiEle[2]->Sumw2();
+    sprintf(histo,"chargeClass_electrons_%d",1);
+    chargeEle[2] = new TH1F(histo, histo, 2, -2., 2.);
+    chargeEle[2]->Sumw2();
+    sprintf(histo,"lhClass_electrons_%d",1);
+    lhEle[2] = new TH1F(histo, histo, 50, 0., 1.);
+    lhEle[2]->Sumw2();
   }
   
 }
@@ -458,9 +525,9 @@ void sPlotsPdfsComparison::bookFullHistos() {
   float HoEMin      =  0.0; // zero-suppression in HCAL
   float HoEMax      =  0.15; // pixelMatchGsfElectron pre-selection: H/E<0.15
   float sigmaIEtaIEtaEBMin = 0.0;
-  float sigmaIEtaIEtaEBMax = 0.03;
-  float sigmaIEtaIEtaEEMin = 0.01;
-  float sigmaIEtaIEtaEEMax = 0.05;
+  float sigmaIEtaIEtaEBMax = 0.035;
+  float sigmaIEtaIEtaEEMin = 0.02;
+  float sigmaIEtaIEtaEEMax = 0.08;
   float sigmaIPhiIPhiEBMin = 0.0;
   float sigmaIPhiIPhiEBMax = 0.03;
   float sigmaIPhiIPhiEEMin = 0.01;
@@ -475,9 +542,10 @@ void sPlotsPdfsComparison::bookFullHistos() {
   else sprintf(hypothesis,"hadrons");
   
   // booking histos eleID
-  // iecal = 0 --> barrel
-  // iecal = 1 --> endcap
-  for (int iecal=0; iecal<2; iecal++) {
+  // iecal = 0 --> barrel |eta|< 0.8
+  // iecal = 1 --> barrel |eta|>= 0.8
+  // iecal = 2 --> endcap
+  for (int iecal=0; iecal<3; iecal++) {
 
     // iptbin = 0: < 15 GeV
     // iptbin = 1: > 15 GeV
@@ -493,7 +561,7 @@ void sPlotsPdfsComparison::bookFullHistos() {
       EoPUnsplitEle[iecal][iptbin]      = new TH1F(histo, histo, nbins, EoPMin, EoPMax);
       sprintf(histo,"HoEUnsplit_%s_subdet%d_ptbin%d",hypothesis,iecal,iptbin);
       HoEUnsplitEle[iecal][iptbin]         = new TH1F(histo, histo, nbins, HoEMin, HoEMax);
-      if(iecal==0) {
+      if(iecal==0 || iecal==1) {
         sprintf(histo,"sigmaIEtaIEtaUnsplit_%s_subdet%d_ptbin%d",hypothesis,iecal,iptbin);
         sigmaIEtaIEtaUnsplitEle[iecal][iptbin] = new TH1F(histo, histo, nbins, sigmaIEtaIEtaEBMin, sigmaIEtaIEtaEBMax);
         sprintf(histo,"sigmaIPhiIPhiUnsplit_%s_subdet%d_ptbin%d",hypothesis,iecal,iptbin);
