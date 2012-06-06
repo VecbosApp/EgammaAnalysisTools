f884558fbda6141f918cb3de01a0862c81f6da52
emanuele <>
2012-06-06 12:35:22 +0000

    added for HLT

--- macro/efficiency/plotTPeff.cc
+++ macro/efficiency/plotTPeff.cc
@@ -17,6 +17,7 @@
 #include <math.h>
 
 #include "plotUtil.cxx"
+#include "collageMuonID_ZZ4L.cxx"
 
 using namespace std;
 
@@ -117,6 +118,102 @@ void plotTPeff() {
       c1.SaveAs(files[i]+TString(".png"));
 
       doRatio(plotdata[i],plotmc[i],files[i],"p_{T} [GeV]",text);
+
+    }
+  }
+
+}
+
+
+
+void plotTPeffHLT() {
+
+  vector<TString> filesData, filesMC;
+  filesData.push_back("Electrons_CBVoigtianPlusChebychevBackground_TnP_Z_DATA_2012_passHLTEle17Leg_Latinos_PtEtaBins.root");
+
+  map<int,TString> etabins;
+  etabins.insert(make_pair(0,"0<|#eta|<1.479"));
+  etabins.insert(make_pair(1,"1.479<|#eta|<2.5"));
+
+  for(unsigned int e=0;e<etabins.size();e++) {
+
+    char filename[100];
+    sprintf(filename,"eff_etabin%d",e);
+    
+    vector<TString> files;
+    files.push_back(filename);
+    
+    TString dirdata = TString("eleIDdir/etapt/fit_eff_plots/");
+    TString dirmc = TString("eleIDdir/etapt/cnt_eff_plots/");
+    char plotname[100];
+    sprintf(plotname,"pt_PLOT_abseta_bin%d_&_passConvR_pass_&_passID_pass_&_passIP_pass_&_passIso_pass",e);
+    TString plot(plotname);
+    
+    TCanvas c1("c1","c1",600,600);
+    c1.SetGridx();
+    c1.SetGridy();
+    TLegend* legend = new TLegend(0.60, 0.16, 0.83, 0.32);
+    legend->SetBorderSize(   0);
+    legend->SetFillColor (   0);
+    legend->SetTextAlign (  12);
+    legend->SetTextFont  (  42);
+    legend->SetTextSize  (0.05);
+    
+    vector<TGraphAsymmErrors*> plotdata, plotmc;
+    for(int i=0;i<(int)filesData.size();++i) {
+      TFile *fileData = TFile::Open(filesData[i]);
+
+      TCanvas *cdata = (TCanvas*)fileData->Get(dirdata+plot);
+      plotdata.push_back((TGraphAsymmErrors*)cdata->FindObject("hxy_fit_eff"));
+
+      c1.cd();
+    
+      plotdata[i]->SetMinimum(0.0);
+      plotdata[i]->SetMaximum(1.2);
+      plotdata[i]->GetXaxis()->SetTitle("p_{T} [GeV]");
+      plotdata[i]->GetYaxis()->SetTitle("efficiency");
+      plotdata[i]->GetXaxis()->SetRangeUser(0,80);
+
+      TF1 *fitFunc = paramTurnOnHLT(plotdata[i], 7, 200, 200);
+      fitFunc->SetLineColor(100); fitFunc->SetLineWidth(2);
+
+      // cosmetics
+
+//       for(int b=0;b<plotdata[i]->GetN();b++) {
+// 	// minos give some large upper error when close to 1. Put the symmetric one from lower error
+// 	float yerrlo=plotdata[i]->GetErrorYlow(b);
+// 	plotdata[i]->SetPointEYhigh(b,yerrlo);
+//       }
+
+      plotdata[i]->SetMarkerSize(1.5);
+      plotdata[i]->SetMarkerStyle(20);
+
+      plotdata[i]->SetLineColor(kAzure-6);
+
+      plotdata[i]->SetMarkerColor(kAzure-6);
+
+      if(i==0) {
+	legend->AddEntry(plotdata[i],"Data 2012","pl");
+      }
+
+      plotdata[i]->Draw("ape");
+      fitFunc->Draw("L SAME");
+      legend->Draw();
+
+      TPaveText* text  = new TPaveText(0.15, 0.9, 0.8, 0.7, "ndc");
+      text->AddText("#sqrt{s} = 8 TeV 2012, L = 1.6 fb^{-1}");
+      text->AddText(etabins[e]);
+      text->SetBorderSize(0);
+      text->SetFillStyle(0);
+      text->SetTextAlign(12);
+      text->SetTextFont(32);
+      text->SetTextSize(0.06);
+      text->Draw();
+
+      c1.SaveAs(files[i]+TString(".pdf"));
+      c1.SaveAs(files[i]+TString(".png"));
+
+      //      doRatio(plotdata[i],plotmc[i],files[i],"p_{T} [GeV]",text);
 
     }
   }
